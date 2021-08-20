4f0d005f83a0ddd6222440eab00414f27c201f3f
yurivaleixo <yuri@valeixo.com>
2021-08-20 01:28:06 -0300

    TESTE DEU CERTO

---
+++ ListaExercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ListaExercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PrimeiroApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o seu nome:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Digite a sua idade:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Nome: {name} e idade: {age}");
+            
+        }
+    }
+}
